15d782cdd75f359fb2dc2853653e23f9ba6bb440
Jon Molnar <jon@cogspace.com>
2021-06-13 02:34:28 -0700

    complete github loader

--- github_loader.lua
+++ github_loader.lua
@@ -1,1 +1,18 @@
-http.get("") -- TODO
+-- GitHub file loader
+
+USER = "cogspace"
+REPO = "computercraft"
+BRANCH = "master"
+FILENAME = "example.lua"
+
+BASE_URL = "https://raw.githubusercontent.com"
+
+local url = BASE_URL .. "/" ..  USER .. "/" .. REPO .. "/" .. BRANCH .. "/" .. FILENAME
+
+local request = http.get( url )
+local response = request.readAll()
+request.close()
+
+local file = fs.open( FILENAME, "w" )
+file.write( response )
+file.close()
