4c6b3b937496509c4c10c1267a400a760942a2a5
Jon Molnar <jon@cogspace.com>
2021-06-13 03:31:56 -0700

    Add comments

--- github.lua
+++ github.lua
@@ -1,6 +1,7 @@
 -- GitHub file loader
 BASE_URL = "https://raw.githubusercontent.com"
 
+-- Parse args
 local args = {...}
 if #args < 3 or #args > 4 then
 	print("Usage: github <user> <repo> <filename> [branch]")
@@ -16,11 +17,12 @@ if not branch then
 	branch = "master"
 end
 
+-- Build URL
 local cacheBuster = math.random(99999999999)
-
 local url = BASE_URL.."/"..user.."/"..repo.."/"..branch.."/"..filename.."?"..cacheBuster
-local request = http.get(url)
 
+-- Download code
+local request = http.get(url)
 if not request then
 	print("Reading from URL failed: " .. url)
 	print("Note: Only public repositories are supported.")
@@ -30,8 +32,10 @@ end
 local response = request.readAll()
 request.close()
 
+-- Write file
 local file = fs.open(filename, "w")
 file.write(response)
 file.close()
 
+-- Print output
 print(("%d bytes written to %s"):format(#response, filename))
