303c4584ecb20fedbf6e5f1dbc1de2b971970b73
Jon Molnar <jon@cogspace.com>
2021-06-15 01:47:31 -0700

    Fix farmer script

--- farmer.lua
+++ farmer.lua
@@ -4,8 +4,8 @@ local WHEAT = "minecraft:wheat"
 local SEEDS = "minecraft:wheat_seeds"
 
 local function cropReady()
-    local data = turtle.inspectDown()
-    return data and data.state and data.state.age == 7
+    local isBlock, data = turtle.inspectDown()
+    return isBlock and data.state and data.state.age == 7
 end
 
 local function farm()
@@ -17,8 +17,6 @@ end
 
 while true do
     -- Wait for the crop to be ready to harvest
-    ---@diagnostic disable-next-line: undefined-field
-    os.sleep(10)
     if cropReady() then
         -- Harvest and replant crop
         t.layer(7, 7, farm)
@@ -31,4 +29,6 @@ while true do
         t.forward(6)
         t.turnAround()
     end
+    ---@diagnostic disable-next-line: undefined-field
+    os.sleep(60)
 end
